a6fcc5601726eaf250c3bec5ecb6d3d07e78485d
whykhdr <98094467+whykhdr@users.noreply.github.com>
2025-05-07 11:13:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -26,10 +26,7 @@ const users = {
 const loginForm = document.getElementById('loginForm');
 const menuContainer = document.getElementById('menuContainer');
 const errorElement = document.getElementById('error');
-const comingSoonContainer = document.getElementById('comingSoonContainer');
-const comingSoonTextElement = document.getElementById('comingSoonText');
 const adminUnitOptionsDiv = document.getElementById('adminUnitOptions');
-const adminOptionsDiv = document.getElementById('adminOptions');
 const userOptionsDiv = document.getElementById('userOptions');
 const logoTextElement = document.querySelector('.container h2');
 const topLogo = document.getElementById('topLogo');
@@ -38,19 +35,6 @@ const topLogo = document.getElementById('topLogo');
 const usernameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
 
-// Tambahkan event listener untuk mendeteksi tombol "Enter"
-usernameInput.addEventListener('keypress', function(event) {
-  if (event.key === 'Enter') {
-    login(); // Panggil fungsi login jika "Enter" ditekan
-  }
-});
-
-passwordInput.addEventListener('keypress', function(event) {
-  if (event.key === 'Enter') {
-    login(); // Panggil fungsi login jika "Enter" ditekan
-  }
-});
-
 // Fungsi login
 function login() {
   const username = usernameInput.value;
@@ -58,10 +42,10 @@ function login() {
 
   if (users.hasOwnProperty(username) && users[username].password === password) {
     localStorage.setItem('loggedInUsername', username);
-    loggedInUsername = username;
     loginForm.classList.add('hidden');
     menuContainer.classList.add('show');
     errorElement.classList.add('hidden');
+    
     if (username === 'admin') {
       adminUnitOptionsDiv.classList.add('show');
     } else {
@@ -75,21 +59,12 @@ function login() {
 
 // Fungsi untuk memperbarui logo
 function updateLogo(user) {
-  if (loggedInUsername) {
-    topLogo.style.display = 'none';
-  } else {
-    topLogo.style.display = 'block';
-  }
-
   logoTextElement.textContent = '';
-
-  if (user === 'admin') {
-    logoTextElement.textContent = 'Pilih Unit';
-  } else if (user && users[user]) {
+  if (user && users[user]) {
     const img = document.createElement('img');
-    img.src = users[user].logo; // Pastikan ini mengarah ke path yang benar
+    img.src = users[user].logo;
     img.alt = users[user].nama;
-    img.style.maxHeight = '100px'; // Ukuran logo diperbesar
+    img.style.maxHeight = '100px';
     img.style.objectFit = 'contain';
     logoTextElement.appendChild(img);
   } else {
@@ -100,7 +75,6 @@ function updateLogo(user) {
 // Fungsi untuk logout
 function logout() {
   localStorage.removeItem('loggedInUsername');
-  loggedInUsername = null;
   loginForm.classList.remove('hidden');
   menuContainer.classList.remove('show');
   errorElement.classList.add('hidden');
